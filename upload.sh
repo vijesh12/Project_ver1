e6e461791ba6ab040125dfa87f999b93599d3872
vijesh1209 <vijesh.vijayan@cevo.com.au>
2018-08-27 09:52:38 +1000

    version01

--- upload.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aws s3 cp cfnlambda s3://vijeshec2cfn
--- upload_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import os.path
-import boto3
-s3 = boto3.resource('s3')
-s3.meta.client.upload_file('./cfnlambda2', 'vijeshec2cfn', 'cfnlambda2')
